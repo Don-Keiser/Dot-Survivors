b82ca87e4090f36adc2e07a83c1c16f4f717e13f
Don Keiser <sworktale@gmail.com>
2025-02-25 09:01:36 +0100

    Reworked GrenadeWeapon
    -Changed from throwing the grenade at random to a clock pattern, as well as adding more grenades with upgrades

--- Dot Survivors/Assets/Scripts/Weapons/Grenade/GrenadeWeapon.cs
+++ Dot Survivors/Assets/Scripts/Weapons/Grenade/GrenadeWeapon.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "GrenadeWeapon", menuName = "ScriptableObjects/GrenadeWeapon", order = 5)]
 public class GrenadeWeapon : WeaponBase
@@ -7,8 +8,9 @@ public class GrenadeWeapon : WeaponBase
     [SerializeField] private float throwRange = 5f;
     [SerializeField] private float explosionRadius = 2f;
     [SerializeField] private float grenadeSpeed = 5f;
-
+    [SerializeField] private int grenadeCount = 1; // Start with 1 grenade
     private float cooldownTimer = 0f;
+    private int lastThrownHour = 0; // Track last thrown hour
 
     public override void UseWeapon(Transform firePoint, Transform player)
     {
@@ -16,23 +18,51 @@ public class GrenadeWeapon : WeaponBase
         if (cooldownTimer <= 0f)
         {
             cooldownTimer = cooldown;
-            ThrowGrenade(firePoint);
+            ThrowGrenades(firePoint);
         }
     }
 
-    private void ThrowGrenade(Transform firePoint)
+    private void ThrowGrenades(Transform firePoint)
     {
-        // Pick a random target point within the range
-        Vector2 targetPoint = (Vector2)firePoint.position + Random.insideUnitCircle * throwRange;
+        List<int> grenadeHours = GetGrenadeHours();
+        
+        foreach (int hour in grenadeHours)
+        {
+            Vector2 targetPoint = GetClockPosition(firePoint.position, hour);
+            GameObject grenade = Instantiate(grenadePrefab, firePoint.position, Quaternion.identity);
+            grenade.GetComponent<Grenade>().Initialize(targetPoint, explosionRadius, damage, grenadeSpeed);
+        }
+    }
 
-        // Create grenade and set its target
-        GameObject grenade = Instantiate(grenadePrefab, firePoint.position, Quaternion.identity);
-        grenade.GetComponent<Grenade>().Initialize(targetPoint, explosionRadius, damage, grenadeSpeed);
+    private List<int> GetGrenadeHours()
+    {
+        List<int> hours = new List<int>();
+
+        // Next hour in the clock cycle (wraps around 1 to 12)
+        lastThrownHour = (lastThrownHour % 12) + 1;
+        hours.Add(lastThrownHour);
+
+        // Add extra grenades in the mirrored backward pattern
+        for (int i = 1; i < grenadeCount; i++)
+        {
+            int mirroredHour = lastThrownHour - i;
+            if (mirroredHour <= 0) mirroredHour += 12; // Wrap around
+            hours.Add(mirroredHour);
+        }
+
+        return hours;
+    }
+
+    private Vector2 GetClockPosition(Vector2 center, int hour)
+    {
+        float angle = (hour - 3) * -30f; // Convert hour to degrees (f.e.: 12 = top, 3 = right)
+        float radian = angle * Mathf.Deg2Rad;
+        return center + new Vector2(Mathf.Cos(radian), Mathf.Sin(radian)) * throwRange;
     }
 
     protected override string[] GetPossibleUpgradeStats()
     {
-        return new string[] { "damageIncrease", "cooldownReduction", "explosionRadiusIncrease" };
+        return new string[] { "damageIncrease", "cooldownReduction", "explosionRadiusIncrease", "extraGrenades" };
     }
 
     protected override void ApplyUpgrade(WeaponUpgradeStep upgrade)
@@ -40,6 +70,7 @@ public class GrenadeWeapon : WeaponBase
         damage += upgrade.GetUpgradeValue("damageIncrease");
         cooldown -= upgrade.GetUpgradeValue("cooldownReduction");
         explosionRadius += upgrade.GetUpgradeValue("explosionRadiusIncrease");
+        grenadeCount += (int)upgrade.GetUpgradeValue("extraGrenades");
     }
 
     public override WeaponBase Clone()
@@ -49,6 +80,7 @@ public class GrenadeWeapon : WeaponBase
         copy.damage = this.damage;
         copy.cooldown = this.cooldown;
         copy.explosionRadius = this.explosionRadius;
+        copy.grenadeCount = this.grenadeCount;
         return copy;
     }
 }
